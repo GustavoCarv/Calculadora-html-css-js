e448e8bfa17d862c80d73150f070b8820ef656c4
GustavoCarv <gustavo.carvg@gmail.com>
2022-08-19 23:37:32 -0300

    fix(keys): multiply operator modified to be calculated

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const KEYS = document.querySelectorAll('[data-action]')
 const NUMBERS = document.querySelectorAll('button:not([data-action])')
 const DISPLAY = document.querySelector('.Calculator__display')
 
-/* console.log("Caiu aqui >>>", NUMBERS) */
+
 
 let operation = '0'
-
 KEYS.forEach((_currentElement, _currentIndex) => {
     _currentElement.addEventListener('click', (e) => {
+
         const ACTION = _currentElement.dataset.action
+        let ACTION_OPERATOR = _currentElement.textContent
 
         if (ACTION === 'calculate') {
-            console.log('Ueeepa')
+            const RESULT = eval(operation)
+            operation = RESULT
+            DISPLAY.textContent = RESULT
+
+
+        } else if (ACTION === 'reset') {
+            operation = '0'
+            DISPLAY.textContent = operation
+        } else {
+            console.log(ACTION_OPERATOR)
+            if (ACTION_OPERATOR === '×') {
+                ACTION_OPERATOR = '*'
+            }
+            if (ACTION_OPERATOR === '÷') {
+                ACTION_OPERATOR = '%'
+            }
+            const newOperation = operation + ACTION_OPERATOR
+            operation = newOperation
+
+            DISPLAY.textContent = operation
         }
     })
 
@@ -20,7 +40,7 @@ KEYS.forEach((_currentElement, _currentIndex) => {
 NUMBERS.forEach((_currentElement, _currentIndex) => {
     _currentElement.addEventListener('click', (e) => {
         const VALUE = e.target.textContent
-        const newOperation = operation + VALUE
+        const newOperation = operation === '0' ? VALUE : operation + VALUE
         operation = newOperation
 
         DISPLAY.textContent = operation
